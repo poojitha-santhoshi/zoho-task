90193eb14da91491b56bd3d9f55654bd4da434eb
poojitha-santhoshi <101345736+poojitha-santhoshi@users.noreply.github.com>
2022-03-13 19:45:21 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,39 @@
+<h1 align="center">
+    <b>Zoho Task </b> 
+<br>
+</h1>
+
+## Getting Started
+
+## Prerequisites
+Tools that we need to run this app:
+
+- ***[Node.js](https://nodejs.org/en/)***
+- ***[Node Package Manager](https://www.npmjs.com/get-npm)***
+- ***[MongoDB](https://www.mongodb.com/try/download/community)***
+
+## Installing
+```
+npm install
+```
+
+## To Run the App
+```
+node server.js
+```
+
+The server will start Running on
++ http://localhost:3000/
+
+
+## Author
+
+| Author                | Profile Link                                       |
+| --------------------- | :------------------------------------------------- |
+| **Poojitha Santhoshi D** | **[Poojitha](https://github.com/poojitha-santhoshi)** |
+
+<br><br>
+
+<p align="center">
+  Made with ❤️ by <a href="https://github.com/guruhariharaun">Poojitha</a>
+</p>
---
+++ routes/index.js
@@ -0,0 +1,133 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+var mongoose = require('mongoose');
+
+router.get('/', function (req, res, next) {
+	return res.render('index.ejs');
+});
+
+
+router.post('/', function(req, res, next) {
+	console.log(req.body);
+	var personInfo = req.body;
+
+
+	if(!personInfo.email || !personInfo.secret || !personInfo.password || !personInfo.passwordConf){
+		res.send();
+	} else {
+		if (personInfo.password == personInfo.passwordConf) {
+
+			User.findOne({email:personInfo.email},function(err,data){
+				if(!data){
+					var c;
+					User.findOne({},function(err,data){
+
+						if (data) {
+							c = data.unique_id + 1;
+						}else{
+							c=1;
+						}
+
+						var newPerson = new User({
+							unique_id:c,
+							email:personInfo.email,
+							secret: personInfo.secret,
+							password: personInfo.password,
+							passwordConf: personInfo.passwordConf
+						});
+
+						newPerson.save(function(err, Person){
+							if(err)
+								console.log(err);
+							else
+								console.log('Success');
+						});
+
+					}).sort({_id: -1}).limit(1);
+					res.send({"Success":"You are regestered,You can login now."});
+				}else{
+					res.send({"Success":"Email is already used."});
+				}
+
+			});
+		}else{
+			res.send({"Success":"password is not matched"});
+		}
+	}
+});
+
+router.get('/login', function (req, res, next) {
+	return res.render('login.ejs');
+});
+
+router.post('/login', function (req, res, next) {
+	User.findOne({email:req.body.email},function(err,data){
+		if(data){
+			
+			if(data.password==req.body.password){
+				req.session.userId = data.unique_id;
+				res.send({"Success":"Success!"});
+				
+			}else{
+				res.send({"Success":"Wrong password!"});
+			}
+		}else{
+			res.send({"Success":"This Email Is not regestered!"});
+		}
+	});
+});
+
+router.get('/profile', function (req, res, next) {
+	User.findOne({unique_id:req.session.userId},function(err,data){
+		
+		if(!data){
+			res.redirect('/');
+		}else{
+			ko.find({}, function (err, allDetails) {
+				if (err) {
+					console.log(err);
+				} else {
+					res.render("data.ejs", { details: allDetails })
+				}
+			})
+		}
+	});
+});
+
+router.get('/logout', function (req, res, next) {
+	console.log("logout")
+	if (req.session) {
+    req.session.destroy(function (err) {
+    	if (err) {
+    		return next(err);
+    	} else {
+    		return res.redirect('/');
+    	}
+    });
+}
+});
+
+
+
+var nameSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    phone: String
+});
+var ko = mongoose.model("ProfileData", nameSchema);
+router.post("/profile", (req, res) => {
+  var myData = new ko(req.body);
+  myData.save()
+      .then(item => {
+		res.redirect('/profile');
+
+
+      })
+      .catch(err => {
+          res.status(400).send("Unable to save to database");
+      });
+});
+
+
+module.exports = router;
